c5f0096bc42641a2c6b3152ffcf66e064e2d8fbb
MikaelMoronta <156107601+MikaelMoronta@users.noreply.github.com>
2024-05-19 16:49:13 -0400

    Javadoc modified main class
    main class was modified to include Javadoc

--- Main.java
+++ Main.java
@@ -1,4 +1,13 @@
+/**
+ * This class contains methods related to the Fibonacci sequence.
+ */
 public class Main {
+    /**
+     * Calculates the nth term of the Fibonacci sequence recursively.
+     *
+     * @param n the position of the term to calculate
+     * @return the nth term of the Fibonacci sequence
+     */
     public static int fibonacci(int n) {
         if (n <= 1)
             return n;
@@ -6,6 +15,11 @@ public class Main {
             return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
+    /**
+     * Main method to demonstrate the usage of the fibonacci method.
+     *
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         int n = 10;
         int result = fibonacci(n);
